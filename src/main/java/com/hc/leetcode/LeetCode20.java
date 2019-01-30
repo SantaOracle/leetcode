81031aa9ec6fe982ac267d74c4bc59cc4cd01351
han.congcong <han.congcong@immomo.com>
2019-01-30 14:56:57 +0800

    结果

--- src/main/java/com/hc/leetcode/LeetCode20.java
+++ src/main/java/com/hc/leetcode/LeetCode20.java
@@ -1,6 +1,8 @@
 package com.hc.leetcode;
 
-
+/**
+ * Runtime: 3 ms, faster than 100.00% of Java online submissions for Valid Parentheses.
+ */
 public class LeetCode20 {
     public boolean isValid(String s) {
 
