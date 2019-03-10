492983f32260c0b9ad69f9e04f60e34cd703df93
congcong.han@qunar.com <congcong.han@qunar.com>
2019-03-10 15:18:37 +0800

    478

---
+++ src/main/java/com/hc/leetcode/Solution.java
@@ -0,0 +1,32 @@
+package com.hc.leetcode;
+
+import java.util.Random;
+
+/**
+ * 478
+ * Created by congcong.han on 2019/3/10.
+ */
+public class Solution {
+    private double radius;
+    private double x_center;
+    private double y_center;
+    private double area;
+    private Random random = new Random();
+
+    public Solution(double radius, double x_center, double y_center) {
+        this.radius = radius;
+        this.x_center = x_center;
+        this.y_center = y_center;
+        this.area = Math.PI * radius * radius;
+    }
+
+    public double[] randPoint() {
+        double jiaodu = random.nextDouble() * 360;
+        double areaS = random.nextDouble() * area;
+        double radiusS = Math.sqrt(areaS / Math.PI);
+        //三角函数
+        double x = Math.cos(jiaodu) * radiusS + x_center;
+        double y = Math.sin(jiaodu) * radiusS + y_center;
+        return new double[]{x,y};
+    }
+}
