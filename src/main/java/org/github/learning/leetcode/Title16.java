ccb4500ecaea273985cd22a71d641f118c73b1e8
SantaOracle <524372977@qq.com>
2019-03-18 18:43:13 +0800

    Title16 init

---
+++ src/main/java/org/github/learning/leetcode/Title16.java
@@ -0,0 +1,14 @@
+package org.github.learning.leetcode;
+
+/**
+ * <a href="https://leetcode.com/problems/3sum-closest/"></a>
+ * Status:
+ * Runtime:
+ * @author peiheng.jiang create on 2019/03/18
+ */
+public class Title16 {
+
+    public int threeSumClosest(int[] nums, int target) {
+        return 0;
+    }
+}
