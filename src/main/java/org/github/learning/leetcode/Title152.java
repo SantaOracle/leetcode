b3eec6208dde574d3bb2dd0ee4132dba56699b70
SantaOracle <524372977@qq.com>
2019-03-19 19:06:02 +0800

    Title152 init

---
+++ src/main/java/org/github/learning/leetcode/Title152.java
@@ -0,0 +1,14 @@
+package org.github.learning.leetcode;
+
+/**
+ * <a href="https://leetcode.com/problems/maximum-product-subarray/"></a>
+ * Status:
+ * Runtime:
+ * @author peiheng.jiang create on 2019/03/19
+ */
+public class Title152 {
+
+    public int maxProduct(int[] nums) {
+        return 0;
+    }
+}
