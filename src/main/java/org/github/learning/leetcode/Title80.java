cd1127b5d469fc78527fe73c25025c7ca509712e
peiheng.jiang <peiheng.jiang@qunar.com>
2019-08-22 21:29:10 +0800

    Title80 accepted

---
+++ src/main/java/org/github/learning/leetcode/Title80.java
@@ -0,0 +1,39 @@
+package org.github.learning.leetcode;
+
+/**
+ * <a href="https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/"></a>
+ * Status:  AC
+ * Runtime: 1 ms, faster than 74.33%
+ * @author peiheng.jiang create on 2019/8/22
+ */
+public class Title80 {
+
+    public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return 1;
+        }
+        int finalIndex = 0;
+        int iteratorIndex = 0;
+        while (iteratorIndex < nums.length) {
+            iteratorIndex++;
+            if (iteratorIndex >= nums.length) {
+                break;
+            }
+            if (finalIndex > 0 && nums[finalIndex] == nums[finalIndex - 1] && nums[finalIndex] == nums[iteratorIndex]) {
+                continue;
+            }
+            finalIndex++;
+            nums[finalIndex] = nums[iteratorIndex];
+        }
+
+        return finalIndex + 1;
+    }
+
+    public static void main(String[] args) {
+        Title80 title80 = new Title80();
+        System.out.println(title80.removeDuplicates(new int[]{1, 1, 1, 2, 2, 5}));
+    }
+}
