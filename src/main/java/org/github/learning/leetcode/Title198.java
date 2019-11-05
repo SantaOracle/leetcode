233a6616110f4a159e65885577fbf7d3a0c714b5
SantaOracle <524372977@qq.com>
2019-11-05 09:58:03 +0800

    update status

--- src/main/java/org/github/learning/leetcode/Title198.java
+++ src/main/java/org/github/learning/leetcode/Title198.java
@@ -4,11 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * <a href="https://leetcode-cn.com/problems/house-robber/"></a>
+ * Status:  AC
+ * Runtime: 2 ms, 在所有 java 提交中击败了5.94%的用户
  * @author peiheng.jiang create on 2019/11/4
  */
 public class Title198 {
-
-    private static Map<Integer, Integer> maxPriceMap = new HashMap<>();
 
     public int rob(int[] nums) {
         if (nums == null || nums.length == 0) {
@@ -17,10 +18,11 @@ public class Title198 {
         if (nums.length == 1) {
             return nums[0];
         }
-        return Math.max(maxPrice(nums, 0), maxPrice(nums, 1));
+        Map<Integer, Integer> maxPriceMap = new HashMap<>();
+        return Math.max(maxPrice(nums, 0, maxPriceMap), maxPrice(nums, 1, maxPriceMap));
     }
 
-    private int maxPrice(int[] nums, int startIndex) {
+    private int maxPrice(int[] nums, int startIndex, Map<Integer, Integer> maxPriceMap) {
         Integer maxPrice = maxPriceMap.get(startIndex);
         if (maxPrice != null) {
             return maxPrice;
@@ -30,7 +32,7 @@ public class Title198 {
         } else {
             maxPrice = Integer.MIN_VALUE;
             for (int i = startIndex + 2; i < nums.length; i++) {
-                maxPrice = Math.max(maxPrice, nums[startIndex] + maxPrice(nums, i));
+                maxPrice = Math.max(maxPrice, nums[startIndex] + maxPrice(nums, i, maxPriceMap));
             }
         }
         maxPriceMap.putIfAbsent(startIndex, maxPrice);
